00961ff0e681c61c5ebe0e2f1f34d8559f58f1af
jinkuen <jinkuen@geothings.tw>
2013-08-17 03:34:25 +0800

    using jade as template engine

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,6 @@
  * GET home page.
  */
 
-var fs = require("fs");
-
 exports.index = function(req, res){
-	var filePath = 'index.html'
-    var encode = 'utf-8';
-
-    res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
-
-    fs.readFile(filePath, encode, function(err, file){
-        res.write(file);
-        res.end();
-    });
+  res.render('index', { title: 'eCourt' });
 };
