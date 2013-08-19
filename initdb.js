1d84f7c41c97f644fdcce937e4ce4d2643024a3e
jinkuen <taiwan.badboy99@gmail.com>
2013-08-20 04:02:18 +0800

    insert fake lawsuits to test.db

--- initdb.js
+++ initdb.js
@@ -5,6 +5,11 @@ var fs = require("fs");
 var dbFile = "test.db";
 var exists = fs.existsSync(dbFile);
 
+if (exists){
+    fs.unlinkSync(dbFile);
+}
+exists = fs.existsSync(dbFile);
+
 if(!exists) {
     console.log("Creating DB file.");
     fs.openSync(dbFile, "w");
@@ -14,10 +19,29 @@ var db = new sqlite3.Database(dbFile);
 
 db.serialize(function() {
     if(!exists) {
-        db.run("CREATE TABLE categories(name TEXT);");
-        var stmt = db.prepare("INSERT INTO categories VALUES (?)");
+        // create db for lawsuits
+        db.run('CREATE TABLE lawsuits(id INTEGER PRIMARY KEY, court TEXT, type TEXT, year INTEGER, word TEXT, num INTEGER, content TEXT);');
+        var stmt = db.prepare("INSERT INTO lawsuits (court, type, year, word, num, content) VALUES (?, ?, ?, ?, ?, ?)");
 
-        //Insert random data
+        for (var i = 0; i < 10; i++) {
+            stmt.run(
+                rand.randstr(6), // court name 
+                rand.randstr(10), // case name
+                rand.randint(1911, 2013), // year
+                rand.randstr(1), // word
+                rand.randint(1, 3000), // num
+                rand.randstr(500)); // content
+        }
+        stmt.finalize();
+
+        db.each("SELECT * FROM lawsuits", function(err, row) {
+            console.log(row);
+        });
+
+        // create db for categories
+        db.run("CREATE TABLE categories(id INTEGER PRIMARY KEY, name TEXT);");
+        var stmt = db.prepare("INSERT INTO categories (name) VALUES (?)");
+
         for (var i = 0; i < 10; i++) {
             stmt.run(rand.randstr(4));
         }
@@ -30,7 +54,6 @@ db.serialize(function() {
 });
 
 db.close();
-
 db = new sqlite3.Database(dbFile);
 
 exports.db = db;
