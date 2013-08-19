51d34ced823576ecb031938aab6f7f9b7f7a7fa2
jinkuen <jinkuen@geothings.tw>
2013-08-19 23:32:32 +0800

    load data from db to jade views

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,11 @@
+var initdb = require("../initdb.js");
+
+var db = initdb.db;
+
 module.exports = function(req, res){
-  res.render('index', { title: 'eCourt' });
+    db.all("SELECT * FROM categories", function(err, rows) {
+        console.log('home.js is called');
+        console.log(rows);
+        res.render('index', { title: 'eCourt', types: rows });
+    });
 };
