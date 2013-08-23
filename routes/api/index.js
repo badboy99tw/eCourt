a8c05c68c403fee576d9af90fd1a0de95006b074
jinkuen <taiwan.badboy99@gmail.com>
2013-08-23 22:50:25 +0800

    require all files in routes/api, except index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,15 @@
-module.exports.categories = require('./categories.js');
-module.exports.groups = require('./groups.js');
-module.exports.lawsuits = require('./lawsuits.js');
+var path = require('path');
+var fs = require("fs");
+
+module.exports = {};
+
+var root = './routes/api/';
+fs.readdirSync(root).forEach(function(file){
+    var name = file.split('.')[0];
+    var ext = path.extname(file);
+
+    if (path.extname(file) != '.js' || file == 'index.js'){
+        return;
+    }
+    module.exports[name] = require('./' + file);
+});
