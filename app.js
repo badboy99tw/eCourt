d277e01e16a85194a4e9c21ec451630c26522973
jinkuen <taiwan.badboy99@gmail.com>
2013-08-26 06:07:01 +0800

    add GET/POST methods for group

--- app.js
+++ app.js
@@ -35,10 +35,12 @@ app.get('/api/category/:category_id/groups', routes.api.category_groups);
 app.get('/api/category/:category_id', routes.api.category);
 app.get('/api/groups', routes.api.groups);
 app.get('/api/group/:group_id/lawsuits', routes.api.group_lawsuits);
+app.get('/api/group/:group_id', routes.api.group.get);
 app.get('/api/lawsuits', routes.api.lawsuits);
 app.get('/api/lawsuit/:lawsuit_id', routes.api.lawsuit.get);
 
 app.post('/api/lawsuit', routes.api.lawsuit.post);
+app.post('/api/group', routes.api.group.post);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
