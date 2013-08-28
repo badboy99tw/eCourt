d1e60feeee97e1556002a04b7c8fe4b2b8dbd251
jinkuen <taiwan.badboy99@gmail.com>
2013-08-28 23:56:44 +0800

    set res.end() instead of return res

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,19 +1,22 @@
 var db = require("../../db.js");
 
 exports.get = function (req, res) {
+    res.end();
 }
 
 exports.listCategories = function (req, res) {
     db.Category.findAll().success(function (categories) {
         res.statusCode = 200;
         res.json(categories);
+        res.end();
     });
 };
 
 exports.createCategory = function (req, res) {
     if (!req.body.hasOwnProperty('title')) {
         res.statusCode = 400;
-        return res.send('Error 400: Post syntax incorrect.');
+        res.send('Error 400: Post syntax incorrect.');
+        res.end();
     }
     db.Category.find({ where: {title: req.body.title}}).success(function (category) {
         if (category === null){
@@ -22,10 +25,12 @@ exports.createCategory = function (req, res) {
             }).success(function (category) {
                 res.statusCode = 200;
                 res.json(category);
+                res.end();
             });
         } else {
             res.statusCode = 400;
-            return res.send('Error 400: duplicate category.');
+            res.send('Error 400: duplicate category.');
+            res.end();
         }
     });
 }
--- routes/api/events.js
+++ routes/api/events.js
@@ -1,13 +1,16 @@
 var db = require("../../db.js");
 
 exports.get = function(req, res){
+    res.statusCode = 404;
+    res.end();
 };
 
 exports.createEvent = function(req, res){
     if (!req.body.hasOwnProperty('title') ||
         !req.body.hasOwnProperty('url')) {
         res.statusCode = 400;
-        return res.send('Error 400: Post syntax incorrect.');
+        res.send('Error 400: Post syntax incorrect.');
+        res.end();
     }
     db.Event.find({ where: {title: req.body.title}}).success(function (event_) {
         if (event_ === null){
@@ -17,10 +20,12 @@ exports.createEvent = function(req, res){
             }).success(function (event_) {
                 res.statusCode = 200;
                 res.json(event_);
+                res.end();
             });
         } else {
             res.statusCode = 400;
-            return res.send('Error 400: duplicate event.');
+            res.send('Error 400: duplicate event.');
+            res.end();
         }
     });
 };
