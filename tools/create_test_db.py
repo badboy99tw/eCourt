4934f1135a876f9a4667128532039be0562fac28
jinkuen <taiwan.badboy99@gmail.com>
2013-08-30 20:01:14 +0800

    create test causes

--- tools/create_test_db.py
+++ tools/create_test_db.py
@@ -26,13 +26,19 @@ class TestDbCreator(object):
         self.robot = UrlRobot(self.url)
         self.robot.connect()
 
-    def createCategory(self):
+    def createCategories(self):
         for title in [u'食品安全', u'自然災害', u'環境污染', u'生態公害', u'司法改革', u'再生能源', u'基本人權', u'文化保護', u'勞資爭議', u'土地正義']:
             category = {'title': title.encode('utf-8')}
-            self.robot.post('/api/categories', category)
+            print self.robot.post('/api/categories', category)
+
+    def createCauses(self):
+        for title in [u'申請假執行', u'申請假扣押', u'申請假處分', u'侵權行為損害賠償', u'請求所有權移轉登記', u'返還不當得利', u'給付工程款', u'禁治產宣告', u'損害賠償', u'不動產所有權移轉登記']:
+            category = {'title': title.encode('utf-8')}
+            print self.robot.post('/api/categories', category)
 
     def run(self):
-        self.createCategory()
+        self.createCategories()
+        self.createCauses()
 
     def close(self):
         self.robot.close()
