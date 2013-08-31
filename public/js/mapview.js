3ca607892ce54da963ea9fbb44fd69387cfa2976
jinkuen <taiwan.badboy99@gmail.com>
2013-08-31 15:03:54 +0800

    add lawsuit markers to map with info window

--- public/js/mapview.js
+++ public/js/mapview.js
@@ -1,6 +1,17 @@
 /*jslint node: true */
 'use strict';
 
+function httpGet(url){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function randint(min, max) {
+    return Math.floor((Math.random() * (max - min)) + min);
+}
+
 function init() {
     var map = new GMaps({
         div: '#map',
@@ -9,12 +20,20 @@ function init() {
         zoom: 7
 	});
 
-    map.addMarker({
-        lat: 23.08,
-        lng: 120.17,
-        title: '台南Room335',
-        infoWindow: {
-            content: '<p>地址</p>'
-        }
-    });
+    var host = 'http://localhost:5566';
+    var lawsuits = httpGet(host + '/api/lawsuits');
+    lawsuits = JSON.parse(lawsuits);
+
+    for (var i in lawsuits) {
+        var lawsuit = lawsuits[i];
+        var lat = randint(2200, 2400);
+        map.addMarker({
+            lat: randint(2190, 2540) / 100.0,
+            lng: randint(12000, 12200) / 100.0,
+            title: lawsuit.cause,
+            infoWindow: {
+                content: '<p>' + lawsuit.title + '</p>'
+            }
+        });
+    }
 }
