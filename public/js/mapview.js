e78213d9f92d2b98afef77f4c18eb7eb2effd192
jinkuen <taiwan.badboy99@gmail.com>
2013-09-01 15:15:43 +0800

    add label to marker, not using google dynamic icon

--- public/js/mapview.js
+++ public/js/mapview.js
@@ -28,10 +28,7 @@ function init() {
         // create marker
         var pos = [randint(2190, 2540)/100.0, randint(12000, 12200)/100.0]
         var marker = L.marker(new L.LatLng(pos[0], pos[1]), {
-            icon: L.icon({
-                iconUrl: 'http://chart.apis.google.com/chart?chst=d_bubble_text_small&chld=bbT|' + event_.title + '|C6EF8C|000000',
-                popupAnchor: [0, 0]
-            }),
+            icon: new L.Icon.Label.Default({ labelText: event_.title }),
             title: event_.title
         });
 
