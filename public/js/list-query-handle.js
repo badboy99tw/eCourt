da5ab6d2ce93f39504afa4cb68efffa82bda60a5
yglin <yglin.mlanser@gmail.com>
2013-09-03 23:48:29 +0800

    Construct query object

--- public/js/list-query-handle.js
+++ public/js/list-query-handle.js
@@ -1,4 +1,29 @@
-function test(title)
+function QueryObject()
 {
-	$('#list-content').html('<p>You clicked ' + title + '</p>');
+	this.category = '土地正義';
+	this.subject = '判決';
+	return this;
+}
+
+var current_query = new QueryObject();
+
+function renderResult()
+{
+	$('#list-content').html('<h2>Query:</h2><p>'
+	 + 'Category = ' + current_query.category + '<br/>'
+	 + 'Subject = ' + current_query.subject
+	 + '</p>'
+	 );
+}
+
+function queryCategory(category)
+{
+	current_query.category = category;
+	renderResult();
+}
+
+function querySubject(subject)
+{
+	current_query.subject = subject;
+	renderResult();
 }
